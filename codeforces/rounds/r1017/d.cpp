b4bd8726f470065fd3a9420ede468e315a0e31e0
derivada <derivadas@protonmail.com>
2025-04-14 13:10:05 +0200

    codeforces round 1017

---
+++ codeforces/rounds/r1017/d.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long	// 64 bits
+#define ld long double	// 80 bits
+#define PI 3.1415926535897932384626433832795l
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<vector<int>> graph;
+typedef vector<vector<pair<int, int>>> wgraph;
+// set with also order_of_key(k) and find_by_order(k)
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define umap unordered_map
+#define uset unordered_set
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define debug(x) cout << #x << " is " << x << endl
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()  // all the structure
+#define F0R(i, n) for (int i = 0; i < n; i++)
+#define R0F(i, n) for (int i = n - 1; i >= 0; i--)
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define ROF(i, a, b) for (int i = b - 1; i >= 0; i--)
+#define fastio cin.sync_with_stdio(0);cin.tie(0);
+const ll MOD = 1e9 + 7; // change MOD value
+
+inline void solve() {
+    string p, s;
+    cin >> p >> s;
+    int n = p.size(), m = s.size(); // m>=n
+    // every sequence of m same letters in a row recorded at p must be reflected in s as a sequence of length m',
+    // m<=m'<=2m
+    int i = 0, j = 0;
+    char letter = p[0];
+    while(i < n && j < m) {
+        // get p seq
+        int curr = 0;
+        while(i < n && p[i] == letter ) {
+            curr++; i++;
+        }
+        // get s seq
+        int curr_s = 0;
+        while(j < m && s[j] == letter) {
+            curr_s++; j++;
+        }
+        if(curr_s < curr || curr_s > 2*curr) {
+            std::cout << "NO\n";
+            return;
+        }
+
+        // change letter
+        if(letter == 'R')
+            letter = 'L';
+        else 
+            letter = 'R';
+    }
+
+    if(i == n && j == m) {
+        std::cout << "YES\n";
+    }
+    else {
+        std::cout << "NO\n";
+    }
+}
+
+int main() {
+    fastio;
+    // freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    int tc;
+    cin >> tc;
+    while (tc--) 
+        solve();
+}
